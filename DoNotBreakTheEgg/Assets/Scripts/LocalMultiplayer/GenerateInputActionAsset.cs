5b31a27e0ee679c7b4fa8ecadd3cf227a2672e44
Cheesemcpuff <joshuahall98@live.com>
2025-01-25 17:46:20 +0000

    Branching to improve local multiplayer system

--- DoNotBreakTheEgg/Assets/Scripts/LocalMultiplayer/GenerateInputActionAsset.cs
+++ DoNotBreakTheEgg/Assets/Scripts/LocalMultiplayer/GenerateInputActionAsset.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class GenerateInputActionAsset : MonoBehaviour
+public class GenerateInputActionAsset : MonoBehaviour, IGeneratedInputActionAsset
 {
-    
+    public IInputActionCollection2 CreateNewGeneratedInputActionAsset()
+    {
+        return new Controls();
+    }
 }
