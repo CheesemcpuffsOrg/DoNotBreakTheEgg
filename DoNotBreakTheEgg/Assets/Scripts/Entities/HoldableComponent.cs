90babf9af4ff8932d3d7d872959a5b4dd199d577
Cheesemcpuff <joshuahall98@live.com>
2025-03-08 20:23:38 +0000

    small protection to make sure holding entities can't be picked up

--- DoNotBreakTheEgg/Assets/Scripts/Entities/HoldableComponent.cs
+++ DoNotBreakTheEgg/Assets/Scripts/Entities/HoldableComponent.cs
@@ -7,16 +7,13 @@ public class HoldableComponent : MonoBehaviour, IHoldableComponent, IInteractabl
 
     [SerializeField] AnchorScriptableObject holdAnchor;
 
-    Rigidbody2D rb;
     IEntity entity;
     IMovementComponent movementComponent;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
         entity = GetComponent<IEntity>();
         movementComponent = GetComponent<MovementComponent>();
-
     }
 
 
@@ -26,10 +23,6 @@ public class HoldableComponent : MonoBehaviour, IHoldableComponent, IInteractabl
 
         transform.position = anchor.position;
         transform.SetParent(anchor);
-
-        /*// Stop the object's movement and set it to Kinematic
-        rb.velocity = Vector2.zero;
-        rb.angularVelocity = 0f; // Also reset any angular velocity (spinning)*/
     }
 
     public void Release()
@@ -40,6 +33,8 @@ public class HoldableComponent : MonoBehaviour, IHoldableComponent, IInteractabl
 
     public void Interact(IEntity interactingEntity)
     {
+        if (HoldEntityManager.Instance.IsEntityHolding(entity)) return;
+
         HoldEntityManager.Instance.AddHeldEntity(interactingEntity, entity, interactingEntity.GetEntityComponent<AnchoringComponent>().GetAnchor(holdAnchor));
     }
 }
