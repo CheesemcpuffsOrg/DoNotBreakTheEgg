3f6fa040c571ac1e0a5e4d971138b5ab2d41da94
Cheesemcpuff <joshuahall98@live.com>
2025-03-10 07:22:48 +0000

    Little cleanup to the break detection to make sure players are ignored

--- DoNotBreakTheEgg/Assets/Scripts/Entities/BreakableComponent.cs
+++ DoNotBreakTheEgg/Assets/Scripts/Entities/BreakableComponent.cs
@@ -17,55 +17,33 @@ public class BreakableComponent : MonoBehaviour, IBreakableComponent
     private void Start()
     {
         entity = GetComponent<IEntity>();
-
-        /*foreach (var registeredEntity in EntityRegistry.RegisteredEntities())
-        {
-            if (registeredEntity == null || !registeredEntity.GetEntityComponent<ITagComponent>().PassTagFilterCheck(ignoreColisionsFilter)) continue;
-
-            IgnoreMainColliderCollisions(registeredEntity);
-        }*/
     }
 
     private void CollisionEnter(Collision2D collision)
     {
-        if(!entity.GetEntityComponent<ITagComponent>().PassTagFilterCheck(breakFilter)) return;
+        if (!entity.GetEntityComponent<ITagComponent>().PassTagFilterCheck(breakFilter)) return;
 
-        //entity.GetEntityComponent<IMovementComponent>().DisableGravity();
+        if(EntityCollisionService.TryGetEntity(collision.collider, out var collisionEntity))
+        {
+            if (collisionEntity.GetEntityComponent<ITagComponent>().PassTagFilterCheck(ignoreColisionsFilter))
+            {
+                EntityCollisionService.IgnoreMainEntityColliders(entity, collisionEntity, true);
+                return;
+            }
+        }
 
         entity.GetEntityComponent<ITagComponent>().AddTag(isDeadTag);
 
         entity.Destroy(1);
     }
 
-    private void TriggerEnter(Collider2D collider)
-    {
-        if (!EntityCollisionService.TryGetEntity(collider, out var collisionEntity)) return;
-
-        if(!collisionEntity.GetEntityComponent<ITagComponent>().PassTagFilterCheck(ignoreColisionsFilter)) return;
-
-        IgnoreMainColliderCollisions(collisionEntity);
-    }
-
-    private void IgnoreMainColliderCollisions(IEntity ignoreEntity)
-    {
-        if (!ignoreEntity.GetEntityComponent<ITagComponent>().PassTagFilterCheck(ignoreColisionsFilter)) return;
-
-        //Debug.Log("Ignore collisions between the main collider of " + entity + " and " + ignoreEntity);
-
-        EntityCollisionService.IgnoreMainEntityColliders(entity, ignoreEntity, true);
-    }
-
     private void OnEnable()
     {
         collision.OnCollisionEnter2D_Action += CollisionEnter;
-        collision.OnTriggerEnter2D_Action += TriggerEnter;
-        //EntityRegistry.EntityRegistered += IgnoreMainColliderCollisions;
     }
 
     private void OnDisable()
     {
         collision.OnCollisionEnter2D_Action -= CollisionEnter;
-        collision.OnTriggerEnter2D_Action -= TriggerEnter;
-       // EntityRegistry.EntityRegistered -= IgnoreMainColliderCollisions;
     }
 }
