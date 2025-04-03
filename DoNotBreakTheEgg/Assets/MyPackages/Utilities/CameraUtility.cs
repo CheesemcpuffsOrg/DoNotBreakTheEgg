7c90ce783f5d3cb48fdc4d4e8bb1283cf6d3350e
Cheesemcpuff <joshuahall98@live.com>
2025-04-03 08:40:47 +0100

    added camera utility script

--- DoNotBreakTheEgg/Assets/MyPackages/Utilities/CameraUtility.cs
+++ DoNotBreakTheEgg/Assets/MyPackages/Utilities/CameraUtility.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraUtility : MonoBehaviour
+public static class CameraUtility
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool IsTransformOutsideView(Camera camera, Transform transform)
     {
-        
-    }
+        if (camera == null || transform == null) return true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 viewportPoint = camera.WorldToViewportPoint(transform.position);
+
+        return viewportPoint.x < 0 || viewportPoint.x > 1 ||
+               viewportPoint.y < 0 || viewportPoint.y > 1 ||
+               viewportPoint.z < 0;
     }
 }
