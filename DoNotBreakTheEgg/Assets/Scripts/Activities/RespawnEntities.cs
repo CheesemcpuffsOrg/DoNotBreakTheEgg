b18b9a99e604a3afde4d0b98e0e760885f3ef85d
Cheesemcpuff <joshuahall98@live.com>
2025-04-05 10:57:46 +0100

    Saving current work to look at adding R3

--- DoNotBreakTheEgg/Assets/Scripts/Activities/RespawnEntities.cs
+++ DoNotBreakTheEgg/Assets/Scripts/Activities/RespawnEntities.cs
@@ -2,14 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RespawnEntities : MonoBehaviour
 {
 
-    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject transporterPadPrefab;
+    [SerializeField] LayerMask spawnLayerMask;
 
     public void RespawnEntity(IEntity entity)
     {
         EntityRegistry.RegisterEntity(entity);
-        entity.GetEntityComponent<IAnchoringComponent>().SetPosition(spawnPoint.position);
+
+        var spawnLocation = CameraUtility.GetRandomLocationOutsideViewport(Camera.main, 1, ViewportSide.XAxis);
+
+        var landingZone = FindLandingZone();
+
+        //tween to the location
+    }
+
+    Vector2 FindLandingZone()
+    {
+        // Get screen bounds in world space
+        Vector2 screenMin = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        Vector2 screenMax = Camera.main.ViewportToWorldPoint(Vector2.one);
+
+        int verticalRayCount = 20;
+        float offset = 0.05f;
+
+        // Spacing between each ray along the top of the screen
+        float verticalRaySpacing = (screenMax.x - screenMin.x) / (verticalRayCount - 1);
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            // Calculate x position for each ray along the top of the screen
+            float xPos = screenMin.x + (i * verticalRaySpacing);
+            Vector2 rayOrigin = new Vector2(xPos, screenMax.y - offset); // Position at the top of the screen
+
+            // Shoot a ray downward
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, Mathf.Infinity, spawnLayerMask);
+
+            Debug.DrawRay(rayOrigin, Vector2.down * 5f, Color.red, 2f); // Visualize in Scene view
+
+            if (hit.collider != null)
+            {
+                return hit.point;
+            }
+        }
+
+        return screenMin;
     }
 }
